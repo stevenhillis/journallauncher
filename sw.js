78d8dab7e195833b24d365e057265d568cf65350
Steven Hillis <hillis.stevenr@gmail.com>
2026-01-04 13:56:31 -0600

    Implement service worker event listeners
    Added install and fetch event listeners for service worker.

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-self.addEventListener("fetch", () => {});
+self.addEventListener('install', (e) => {
+  // Logic for caching if you want offline support
+});
+
+self.addEventListener('fetch', (e) => {
+  // This can be empty, but the listener MUST exist
+});
